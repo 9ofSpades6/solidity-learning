75c252a2b2bd6520a59f101681c9384012399cff
9ofSpades6 <anxihj24@gmail.com>
2025-10-13 00:54:24 +0900

    added approve and transferFrom

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,6 +9,8 @@ pragma solidity ^0.8.28; // 컴파일러에게 소스 코드를 어떻게 처리
 
 // class 대신 contract
 contract MyToken {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed spender, uint256 amount);
     string public name;
     string public symbol;
     uint8 public decimals; // 1ETH --> 1*10^18 wei, 1 wei --> 1*10^(-18)
@@ -16,8 +18,8 @@ contract MyToken {
     // uint8 --> 8bit unsigned int ...., uint256 (32byte)
     uint256 public totalSupply; //총 발행량
     mapping(address => uint256) public balanceOf; // 누가 얼마를 가지고 있느냐
-
     // 데이터를 조회하는 것은 tx로 처리되지 않음. 그냥 api call로 리턴해줌
+    mapping(address => mapping(address => uint256)) allowance;
 
     constructor(
         string memory _name,
@@ -35,11 +37,26 @@ contract MyToken {
     //from, to, data, value, gas,...
     // 생성자를 불러올때만 배포되니까 totalSupply는 영원히 1이겠징 추가발행을 금지하려면 이렇게...
 
+    function approve(address spender, uint amount) external {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(spender, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external {
+        address spender = msg.sender;
+        require(allowance[from][spender] >= amount, "insufficient allowance");
+        allowance[from][spender] -= amount;
+        balanceOf[from] -= amount; // 이부분이 취약
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
     function _mint(uint256 amount, address owner) internal {
         totalSupply += amount; //누군가에게 발행한 토큰을 안넣어주면 토큰이 증발함
         balanceOf[owner] += amount;
         // totalSupply = totalSupply + amount;
         // balanceOf[owner] = balanceOf[owner] + amount;
+        emit Transfer(address(0), owner, amount);
     }
 
     // function totalSupply() external view returns (uint256) {
@@ -56,5 +73,7 @@ contract MyToken {
         require(balanceOf[msg.sender] >= amount, "insufficient balance");
         balanceOf[msg.sender] -= amount; //가지고 있는 토큰보다 더 많은 양을 보내게 되면?? 오버플로 발생! uint라서 음수가 만들어질 수 없는데 음수를 만들려한 거임
         balanceOf[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
     }
 }
