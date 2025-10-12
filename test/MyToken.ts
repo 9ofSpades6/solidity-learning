41d844d8545f2c8370a276b1e90eebbbb0f56e4a
9ofSpades6 <anxihj24@gmail.com>
2025-10-12 23:15:46 +0900

    transfer test

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -6,20 +6,22 @@ import { expect } from "chai"
 import { MyToken } from "../typechain-types";
 import {HardhatEthersSigner} from "@nomicfoundation/hardhat-ethers/signers";
 
-describe("mytoken deploy", () => {
+const mintingAmount = 100n;
+const decimals = 18n;
+
+describe("My Token", () => {
     let myTokenC:MyToken;
     let signers: HardhatEthersSigner[]; //여러 테스트 월렛이 필요할 수 있으니까 
-    before("should deploy", async ()=> {
+    beforeEach("should deploy", async ()=> {
         signers = await hre.ethers.getSigners();
         myTokenC = await hre.ethers.deployContract("MyToken", [
             "MyToken",
             "MT",
-            18,
+            decimals,
+            100,
         ]);
-        expect(await myTokenC.name()).equal("MyToken");
-        expect(await myTokenC.symbol()).equal("MT");
-        expect(await myTokenC.decimals()).equal("18");
     });
+    describe("Basic state value check", () => {
     it("should return name", async () => {
         expect(await myTokenC.name()).equal("MyToken");
     });
@@ -27,15 +29,38 @@ describe("mytoken deploy", () => {
          expect(await myTokenC.symbol()).equal("MT");
     });
     it("should return decimals", async () => {
-        expect(await myTokenC.decimals()).equal("18");
+        expect(await myTokenC.decimals()).equal(decimals);
     });
-    it("should return 0 totalSupply", async ()=> {
-        expect(await myTokenC.totalSupply()).equal(1n*10n**18n);
-    })    
+    it("should return 100 totalSupply", async ()=> {
+        expect(await myTokenC.totalSupply()).equal(mintingAmount * 10n ** decimals);
+        });    
+    });
+
+    describe("Mint", () => {
     it("should return 1MT balance for signer 0", async () => {
         const signer0 = signers[0];
-        expect(await myTokenC.balanceOf(signer0)).equal(1n*10n**18n);
+        expect(await myTokenC.balanceOf(signer0)).equal(mintingAmount*10n**decimals);
+        });
+    });
+    describe("Transfer", () => {
+    it("should have 0.5MT", async () => {
+        const signer1 = signers[1];
+        await myTokenC.transfer(hre.ethers.parseUnits("0.5", decimals), signer1.address);
+        console.log(await myTokenC.balanceOf(signer1));
+        expect(await myTokenC.balanceOf(signer1)).equal(
+            hre.ethers.parseUnits("0.5", decimals)
+        ); 
+    });
+    it("should be reverted with insufficient balance error", async () => {
+        const signer1 = signers[1];
+        await expect(
+        myTokenC.transfer(
+            hre.ethers.parseUnits((mintingAmount +1n).toString(), decimals), 
+            signer1.address
+           )
+        ).to.be.revertedWith("insufficient balance");
+        });
     });
 });
-
 //로직에 대한 테스트를 잘 마련해놓으면 오류를 잡기 쉽다.
+//종속성 있는건 끊어내기
