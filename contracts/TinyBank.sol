291182285ab1d219c662c06f95d2e7a4c26ef6e8
9ofSpades6 <anxihj24@gmail.com>
2025-11-30 04:08:56 +0900

    revert TinyBank to use ManagedAcess instead of MultiManagedAccess

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -20,7 +20,7 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
-import "./MultiManagedAccess.sol";
+import "./ManagedAccess.sol";
 
 interface IMyToken {
     function transfer(uint256 amount, address to) external;
@@ -30,7 +30,7 @@ interface IMyToken {
     function mint(uint256 amount, address owner) external;
 }
 
-contract TinyBank is MultiManagedAccess {
+contract TinyBank is ManagedAccess {
     event Staked(address, uint256);
     event Withdraw(uint256 amount, address to);
 
@@ -44,11 +44,7 @@ contract TinyBank is MultiManagedAccess {
     mapping(address => uint256) public staked;
     uint256 public totalStaked; //myToken의 totalsupply와 balanceof와 헷갈림. 이건 tinybank 서비스를 이용하는 사람들이 예치해놓은 토큰량의 합. totalsupply는 각자 가지고 있는거.
 
-    constructor(
-        address[] memory _managers,
-        uint _manager_numbers,
-        IMyToken _stakingToken
-    ) MultiManagedAccess(msg.sender, _managers, _manager_numbers) {
+    constructor(IMyToken _stakingToken) ManagedAccess(msg.sender, msg.sender) {
         stakingToken = _stakingToken;
         rewardPerBlock = defaultRewardPerBlock;
     }
@@ -66,9 +62,8 @@ contract TinyBank is MultiManagedAccess {
         _; //어떤 함수가 modifier를 호출하고 싳으면 그 앞에 내용을 포함하라는 것. caller's code
     }
 
-    function setRewardPerBlock(uint256 _amount) external onlyAllConfirmed {
+    function setRewardPerBlock(uint256 _amount) external onlyManager {
         rewardPerBlock = _amount;
-        reset();
     }
 
     function stake(uint256 _amount) external updateReward(msg.sender) {
--- test/TinyBank.ts
+++ test/TinyBank.ts
@@ -8,27 +8,15 @@ describe("TinyBank", () => {
     let signers: HardhatEthersSigner[];
     let myTokenC: MyToken;
     let tinyBankC: TinyBank;
-    let owner: HardhatEthersSigner;
-    let managers: HardhatEthersSigner[];
-    let nonManager: HardhatEthersSigner;
-
     beforeEach(async () => {
         signers = await hre.ethers.getSigners();
-        owner = signers[0];
-        managers = [signers[1], signers[2], signers[3], signers[4], signers[5]];
-        nonManager = signers[6];
          myTokenC = await hre.ethers.deployContract("MyToken", [
             "MyToken",
             "MT",
             DECIMALS,
             MINTING_AMOUNT,
         ]);
-
-        const managerAddresses = managers.map(m => m.address);
-
         tinyBankC = await hre.ethers.deployContract("TinyBank", [
-            managerAddresses,
-            5,
             await myTokenC.getAddress()
         ]);
         await myTokenC.setManager(tinyBankC.getAddress());
@@ -49,9 +37,7 @@ describe("TinyBank", () => {
             const signer0 = signers[0];
             const stakingAmount = hre.ethers.parseUnits("50", DECIMALS);
             await myTokenC.approve(await tinyBankC.getAddress(), stakingAmount);
-            await expect(tinyBankC.stake(stakingAmount)).to.emit(tinyBankC, "Staked").withArgs(
-                signer0.address, stakingAmount
-            );
+            await tinyBankC.stake(stakingAmount);
             expect(await tinyBankC.staked(signer0.address)).equal(stakingAmount);
             expect(await tinyBankC.totalStaked()).equal(stakingAmount);
             expect(await myTokenC.balanceOf(tinyBankC)).equal(
@@ -66,9 +52,7 @@ describe("TinyBank", () => {
             const stakingAmount = hre.ethers.parseUnits("50", DECIMALS);
             await myTokenC.approve(await tinyBankC.getAddress(), stakingAmount);
             await tinyBankC.stake(stakingAmount);
-            await expect(tinyBankC.withdraw(stakingAmount))
-            .to.emit(tinyBankC, "Withdraw")
-            .withArgs(stakingAmount, signer0.address);
+            await tinyBankC.withdraw(stakingAmount);
             expect(await tinyBankC.staked(signer0.address)).equal(0);
             });
         });
@@ -92,41 +76,12 @@ describe("TinyBank", () => {
                     hre.ethers.parseUnits((BLOCKS + MINTING_AMOUNT + 1n).toString()) 
                 );
             });
-        });
-
-        describe("Multi Manager Access Control", () => {
-            describe("Confirm function", () => {
-                it("should allow managers to confirm", async () => {
-                    await tinyBankC.connect(managers[0]).confirm();
-                    expect(await tinyBankC.confirmed(0)).equal(true);
-                });
-
-                it("should revert when non-manager tries to confirm", async () => {
-                    await expect(
-                        tinyBankC.connect(nonManager).confirm()
-                    ).to.be.revertedWith("You are not a manager");
-                });
-
-                it("should allow all managers to confirm", async () => {
-                for (let i = 0; i < 5; i++) {
-                    await tinyBankC.connect(managers[i]).confirm();
-                    expect(await tinyBankC.confirmed(i)).equal(true);
-                }
+            it("Should revert when changing rewardPerBlock by hacker", async () => {
+                const hacker = signers[3];
+                const rewardToChange = hre.ethers.parseUnits("10000", DECIMALS);
+                await expect(tinyBankC.connect(hacker).setRewardPerBlock(rewardToChange)).to.be.revertedWith(
+                    "You are not authorized to manage this contract"
+                );
             });
-         });
-
-         describe("setRewardPerBlock with MultiManagedAccess", () => {
-            it("should revert when not all managers have confirmed", async () => {
-                const newReward = hre.ethers.parseUnits("10", DECIMALS);
-                
-                await tinyBankC.connect(managers[0]).confirm();
-                await tinyBankC.connect(managers[1]).confirm();
-                await tinyBankC.connect(managers[2]).confirm();
-                
-                await expect(
-                    tinyBankC.connect(managers[0]).setRewardPerBlock(newReward)
-                ).to.be.revertedWith("Not all confirmed yet");
-            });
-         });
         });
     });
