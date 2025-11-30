9096482b60fbd4853831c3e932869678265d73f1
9ofSpades6 <anxihj24@gmail.com>
2025-11-30 15:31:36 +0900

    final assignment. added  TB, MT.sol, deploy.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,6 +7,27 @@ const config: HardhatUserConfig = {
   vyper: {
     version: "0.3.10",
   },
+  networks: {
+    kairos: {
+      url:"https://public-en-kairos.node.kaia.io",
+      accounts: ["0x3de47ad52eda235fbab25931a4f2723e621d9c75e0298adc3e8b63aa8fe7910e"],
+    },
+  },
+  etherscan: {
+      apiKey: {
+        kairos: "unnecessary",
+      },
+      customChains: [
+        {
+          network: "kairos",
+          chainId: 1001,
+          urls: {
+            apiURL: "https://kairos-api.kaiascan.io/hardhat-verify",
+            browserURL: "https://kairos.kaiascan.io",
+          },
+        },
+      ],
+    },
 };
 
 export default config;
--- ignition/modules/Deploy.ts
+++ ignition/modules/Deploy.ts
@@ -3,9 +3,12 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("MyTokenDeploy", (m) => {
-  const myTokenC = m.contract("MyToken", ["MyToken", "MT", 18])
-  return { myTokenC };  
+  const myTokenC = m.contract("MyToken", ["MyToken", "MT", 18, 100]);
+  const tinyBankC = m.contract("TinyBank", [myTokenC]);
+  m.call(myTokenC, "setManager", [tinyBankC]);
+  return { myTokenC, tinyBankC };
 });
+
 
 // 근데 이거 블록체인 네트워크 안띄웠는데 어디에 배포한거임? 배포하면서 일시적으로 하드햇 네트워크 띄우고 배포하고 또 하드햇 네트워크 종료한거임.
 // 그럼 하드헷 네트워크 다시 띄우면 배포된 컨트랙트가 없을거임. 
